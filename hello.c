d2729a1f8b2a0011499dbbadcebdfd6855409ce8
SIVARANJAN S <132982317+Siva-Ranjan@users.noreply.github.com>
2024-02-08 17:14:53 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
-int fact(){
-if(i == 0 && i == 1){
+int fact(int i){
+if(i == 0 || i == 1){
 return 1;}
 else{
-return i * fact(i - 1);
+return (i * fact(i - 1));
 }
 }
 
 
 int main(){
-int i = 5;
-int result = fact(i);
-printf("The factorial of %d is: %d\n", i, result);
+int n = 5;
+int result = fact(n);
+printf("The factorial of %d is: %d\n", n, result);
 return 0;
 }
